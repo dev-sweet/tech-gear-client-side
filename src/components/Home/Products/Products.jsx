4eee643099209d09576c31c481906b67b5628579
dev-sweet <sweetali0520@gmail.com>
2025-02-20 12:05:47 +0600

    homepage product and selection completed

---
+++ src/components/Home/Products/Products.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import ProductCard from "../ProductCard/ProductCard";
+const tabs = [
+  { id: "all", label: "All" },
+  { id: "laptop", label: "Laptop" },
+  {
+    id: "phone",
+    label: "Phone",
+  },
+  { id: "gaming", label: "Gaming" },
+  { id: "music", label: "Music" },
+];
+const Products = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+  //   const [products, setProducts] = useState(null);
+  const products = [
+    {
+      id: 1,
+      name: "Apple iPhone 15 Pro Max",
+      image: "https://example.com/iphone15pro.jpg",
+      price: 1199,
+      discountPrice: 1050,
+      ratings: 4.9,
+    },
+    {
+      id: 2,
+      name: "Sony WH-1000XM5 Headphones",
+      image: "https://example.com/sony-wh1000xm5.jpg",
+      price: 399,
+      discountPrice: 308,
+      ratings: 4.7,
+    },
+    {
+      id: 3,
+      name: "Dell XPS 15 Laptop",
+      image: "https://example.com/dell-xps15.jpg",
+      price: 1699,
+      ratings: 4.8,
+    },
+    {
+      id: 4,
+      name: 'Samsung 65" QLED 4K Smart TV',
+      image: "https://example.com/samsung-qled65.jpg",
+      price: 1299,
+      ratings: 4.6,
+    },
+    {
+      id: 5,
+      name: "Apple Watch Series 9",
+      image: "https://example.com/apple-watch9.jpg",
+      price: 429,
+      ratings: 4.7,
+    },
+  ];
+
+  const handleClick = (id) => {
+    setActiveTab(id);
+    // setProducts("");
+  };
+  return (
+    <div className="mx-auto px-20 mt-5">
+      {/* Tab Navigation */}
+      <div className="flex space-x-2">
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => handleClick(tab.id)}
+            className={`py-2 px-4 transition-all hover:cursor-pointer ${
+              activeTab === tab.id
+                ? "bg-gray-800 border-b-2 text-white font-semibold"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+
+      {/* Tab Content */}
+      <div className="mt-4 p-4">
+        {/* {tabs.find((tab) => tab.id === activeTab)?.content} */}
+        <div className="grid grid-cols-5 gap-5 h-full">
+          {products.map((product) => (
+            <ProductCard
+              className="h-full"
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Banner from "../../components/Home/Banner/Banner";
 import Features from "../../components/Home/Features/Features";
+import Newsletter from "../../components/Home/Newsletter/Newsletter";
+import Products from "../../components/Home/Products/Products";
+import Selection from "../../components/Home/Selection/Selection";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Features />
+      <Products />
+      <Selection />
+      <Newsletter />
     </>
   );
 };
