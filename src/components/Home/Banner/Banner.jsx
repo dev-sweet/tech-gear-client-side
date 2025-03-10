ab9e6fd06ea6e477e5f25718126252f298be233e
dev-sweet <sweetali0520@gmail.com>
2025-03-10 17:43:18 +0600

    modified banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -16,34 +16,32 @@ const Banner = () => {
       autoFocus={true}
       emulateTouch={true}
     >
-      <div className="banner-full lg:py-0 md:py-0 py-10">
+      <div className="banner-bg lg:py-0 md:py-0 py-10 ">
         <div className="flex lg:flex-row md:flex-row flex-col items-center justify-between min-h-full">
-          <div className="lg:pl-20 pl-10 text-left lg:w-[40%] w-full flex flex-col md:items-start items-center">
-            <h1 className="lg:text-5xl md:text-4xl text-2xl lg:text-left md:text-left text-center font-semibold">
+          <div className="lg:pl-20 pl-10 text-left lg:w-[40%] w-full">
+            <h1 className="text-gray-100 lg:text-5xl md:text-4xl text-2xl font-semibold">
               Unleash Power with the <br className="" /> New MacBook!
             </h1>
-            <button className="md:mt-5 mt-3 lg:text-xl  border-b-1 border-gray-500 text-gray-500 font-semibold hover:border-gray-700 hover:text-gray-700 transition cursor-pointer flex-start">
+            <button className="md:mt-5 mt-3 lg:text-xl border-b-1 border-gray-100 text-gray-100 font-semibold hover:border-gray-500 hover:text-gray-500 transition cursor-pointer flex-start">
               Shop Now
             </button>
           </div>
-          <div className="relative py-12 lg:pr-20 pr-10">
+          <div className="py-12 lg:pr-20 pr-10">
             <img className="banner-img" src={bannerImg1} />
-            <div className="banner-bg-color"></div>
           </div>
         </div>
       </div>
-      <div className="banner-full lg:py-0 md:py-0 py-10 min-h-full">
-        <div className="lg:pl-20 pl-10 flex lg:flex-row md:flex-row flex-col items-center justify-between">
-          <div className="text-left lg:w-[40%] w-full h-full">
-            <div className="banner-bg-color-2"></div>
-            <h1 className="lg:text-5xl md:text-4xl text-2xl font-semibold">
-              Porto Touch Laptop <br /> Pro Edition 2
+      <div className="lg:py-0 md:py-0 py-10 lg:px-20 px-10 banner-bg2  min-h-full">
+        <div className="lg:pt-10 flex md:flex-row flex-col items-center justify-between">
+          <div className="text-left lg:w-[40%] w-full">
+            <h1 className="text-gray-100 lg:text-5xl md:text-4xl text-2xl font-semibold">
+              Limited Time Deal – <br /> Grab Your Apple Watch Today!
             </h1>
-            <button className="mt-5 px-4 text-xl py-2 border-b-2 border-gray-500 text-gray-500 font-semibold hover:border-gray-700 hover:text-gray-700 transition cursor-pointer">
+            <button className="md:mt-5 mt-3 lg:text-xl border-b-1 border-gray-100 text-gray-100 font-semibold hover:border-gray-500 hover:text-gray-500 transition cursor-pointer flex-start">
               Shop Electronics
             </button>
           </div>
-          <div className="py-12 lg:pr-20 pr-10">
+          <div className="py-12">
             <img className="banner-img" src={bannerImg2} />
           </div>
         </div>
--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -35,6 +35,7 @@ const AdminHome = () => {
     },
   });
 
+  console.log(adminStats, chartData);
   // custom bar chart
   const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
